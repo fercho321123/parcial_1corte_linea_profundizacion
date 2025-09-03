827dba04236cea6a5fa88f0c19e41d41827a54fe
fernando jimenez <brayanjimenez3627@gmail.com>
2025-09-03 14:55:39 -0500

    configurar credneciales base de datos en postgres

--- parcial/settings.py
+++ parcial/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'parcial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Biblioteca',
+        'USER':'postgres',
+        'PASSWORD':'KCRT6_]mt6J;.:s+kew',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
