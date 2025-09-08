11fecbff3066a65b44409c9c6ae34e76b157a182
fernando jimenez <brayanjimenez3627@gmail.com>
2025-09-08 15:23:27 -0500

    ajustes finales ahora si la buena

--- parcial/urls.py
+++ parcial/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('catalogo.urls')),
+    path('api/', include('api_app.urls')),
 ]
